e3e376f8be56fad3c3ed48d7244b09bedc9b3b87
Lily Zihui Zhu <zhuzihui.lily@gmail.com>
2022-07-28 23:49:56 -0400

    added instantiation code and tests for HMM

--- models.py
+++ models.py
@@ -1,6 +1,33 @@
+"""
+This script instantiates objects relevant to HMM clustering.
+
+Relationship among the objects below:
+(1) Each MultivariateGaussian is an individual Gaussian distribution.
+(2) Each GaussianMixture (GMM) is a weighted collection of the single Gaussians.
+    Each GMM describes the distribution of a particular "state".
+(3) Each HiddenMarkovModel (HMM) is a collection of the GMMs.
+    An HMM describes how the observations represents transitions among different states.
+    The most likely "state" for a particular observation depends on both the previous state and 
+    whichever state results in higher likelihood of the current observation.
+(4) A HiddenMarkovMixtureModel (H3M) is a collection of HMMs.
+    To accurately describe the state transition behavior of a particular subject,
+    multiple trials are needed,
+    and each HMM describes the state transition behavior of a particular trial.
+
+Meaning of common object attributes:
+(1) n_feature:  number of features tracked for each observation.
+                E.g., n_feature=2 if both x- and y-coordinates are tracked.
+(2) n_mix:  number of mixtures (multivariate Gaussian objects) in each Gaussian Mixture Model. 
+            For simplicity, assume n_mix is constant for all GMMs (in all states).
+(3) n_component:    number of components in each Hidden Markov Model. Essentially, 
+                    number of states tracked by each HMM and
+                    number of GMM contained in each HMM
+                    because each GMM describes the distribution of each state.
+"""
 from hmmlearn.hmm import GMMHMM
 import numpy as np
 from scipy.stats._multivariate import multivariate_normal_frozen
+
 
 class MultivariateGaussian(multivariate_normal_frozen):
     """Extends a scipy.stats.multivariate_normal object."""
@@ -16,26 +43,27 @@ class MultivariateGaussian(multivariate_normal_frozen):
         :type cov: array_like (numerical)
         """
         super().__init__(mean=mean, cov=cov)
-        self.dimension = len(mean)
+        self.n_features = len(mean)
 
-class GaussianMixture(object):
+
+class GaussianMixtureModel(object):
     """Implements a Gaussian Mixture Model."""
 
-    def __init__(self, components, weights):
+    def __init__(self, mixtures, weights):
         """
         Initializes a Gaussian Mixture object.
 
-        :param component: A list of individual MultivariateGaussian objects.
-        :type components: array_like (MultivariateGaussian)
+        :param mixtures: A list of individual MultivariateGaussian objects.
+        :type mixtures: list (MultivariateGaussian)
 
         :param weights: A list of weights of the corresponding Gaussian component.
                         The weights should sum to one and have the same length as `components`.
         :type weights: array_like (numerical)
         """
-        self.components = components
+        self.mixtures = mixtures
         self.weights = weights
-        self.n_components = len(components)
-        self.dimension = components[0].dimension
+        self.n_mix = len(mixtures)
+        self.n_features = mixtures[0].n_features
 
     def _pdf(self, x):
         """
@@ -44,10 +72,10 @@ class GaussianMixture(object):
         :param x: The input coordinate (could be a scalar or a list of scalars).
         :type x: array_like or single numerical
 
-        :return: The pdf value for the single coordinate.
+        :returns: The pdf value for the single coordinate.
         :rtype: numerical
         """
-        return np.sum([self.weights[i] * self.components[i].pdf(x) for i in range(len(self.components))])
+        return np.sum([self.weights[i] * self.mixtures[i].pdf(x) for i in range(self.n_mix)])
 
     def pdf(self, x):
         """
@@ -56,42 +84,63 @@ class GaussianMixture(object):
         :param x: A list of input coordinates for whom the pdf values will be calculated.
         :type x: array_like (numerical)
 
-        :return: A list of output pdf values (same length as x).
+        :returns: A list of output pdf values (same length as x).
         :rtype: numpy.array (numerical)
         """
         return np.array([self._pdf(xi) for xi in x])
     
     def get_means(self):
-        """Returns the set of all means with shape (n_components, dimension)."""
-        flat_means = np.array([g.mean for g in self.components])
-        return flat_means.reshape((self.n_components, self.dimension))
+        """Returns the set of all means with shape (n_mix, n_feature)."""
+        flat_means = np.array([g.mean for g in self.mixtures])
+        return flat_means.reshape((self.n_mix, self.n_features))
 
     def get_covs(self):
-        """Returns the set of all covariances with shape (n_components, dimension, dimension)."""
-        flat_covs = np.array([g.cov for g in self.components])
-        return flat_covs.reshape((self.n_components, self.dimension, self.dimension))
+        """Returns the set of all covariances with shape (n_mix, n_feature, n_feature)."""
+        flat_covs = np.array([g.cov for g in self.mixtures])
+        return flat_covs.reshape((self.n_mix, self.n_features, self.n_features))
     
 
 class HiddenMarkovModel(GMMHMM):
-    """Extends a hmmlearn.hmm.GMMHMM object (Gaussian Mixture HMM)."""
-    def __init__(self, components, startprob, transmat):
-        super().__init__()
-        # GMMHMM parameters
-        self.n_components = len(components)
-        self.n_mix = components[0].n_components
-        self.covariance_type = "full"
-        self.startprob_ = startprob
-        self.transmat_ = transmat
 
-        self.weights_ = np.array([gmm.weights for gmm in components]).\
-                        reshape((self.n_components,self.n_mix))
-        self.means_ = np.array([gmm.get_means() for gmm in components]).\
-                      reshape(self.n_components,self.n_mix,components[0].dimension)
-        self.covars_ = np.array([gmm.get_covs() for gmm in components]).\
-                       reshape(self.n_components,self.n_mix,components[0].dimension,components[0].dimension)
+    """Extends a hmmlearn.hmm.GMMHMM object (Gaussian Mixture HMM)."""
+
+    def __init__(self, components, startprob, transmat):
+        """
+        Initializes a Hidden Markov Model object.
+        For this algorithm, assume all GMM components have the same number of mixtures (n_mix).
+
+        :param components: The list of GMM objects describing the state distributions.
+        :type components: list (of GaussianMixtureModel())
+
+        :param startprob: The vector of probabilities of starting in each of the states.
+                          len(startprob) = len(components)
+        :type startprob: array_like (numerical)
+
+        :param transmat: The transition matrix describing the probabilities of transitioning between states.
+                         transmat.shape = (len(components) x len(components))
+        :type transmat: array_like (numerical)
+        """
+        # GMMHMM initialization parameters
+        kwargs = {
+            "n_components": len(components),
+            "n_mix": components[0].n_mix, # Assume all GMMs have the same number of mixtures for simplicity
+            "covariance_type": "full",
+        }
+        super().__init__(**kwargs)
 
         # Child class specific parameters
         self.components = components
+        self.n_features = components[0].n_features
+
+        # Set (GMMHMM) instance variables
+        self.startprob_ = startprob 
+        self.transmat_ = transmat
+        self.weights_ = np.array([gmm.weights for gmm in components]).\
+                        reshape((self.n_components, self.n_mix))
+        self.means_ = np.array([gmm.get_means() for gmm in components]).\
+                      reshape(self.n_components, self.n_mix, self.n_features)
+        self.covars_ = np.array([gmm.get_covs() for gmm in components]).\
+                       reshape(self.n_components, self.n_mix, self.n_features, self.n_features)
 
 
 class HiddenMarkovMixtureModel(object):
--- test_models.py
+++ test_models.py
@@ -1,3 +1,4 @@
+from operator import mod
 import models 
 import numpy as np
 from scipy.stats import multivariate_normal
@@ -26,21 +27,21 @@ class TestModels(unittest.TestCase):
         assert np.allclose(test_pdf, scipy_pdf)
 
     def test_init_gmm(self):
-        """Tests if the instance variables are correctly set for GaussianMixture()."""
+        """Tests if the instance variables are correctly set for GaussianMixtureModel()."""
         gaussian_1 = models.MultivariateGaussian(mean=[0, 0], cov=[[1, 0], [0, 1]])
         gaussian_2 = models.MultivariateGaussian(mean=[1, 1], cov=[[2, 0], [0, 2]])
         gaussian_3 = models.MultivariateGaussian(mean=[99, 99], cov=[[2, 1], [1, 2]])
         gaussians = [gaussian_1, gaussian_2, gaussian_3]
         weights = [0.7, 0.2, 0.1]
-        test_gmm = models.GaussianMixture(components=gaussians, weights=weights)
+        test_gmm = models.GaussianMixtureModel(gaussians, weights)
 
-        self.assertListEqual(test_gmm.components, gaussians)
+        self.assertListEqual(test_gmm.mixtures, gaussians)
         self.assertListEqual(test_gmm.weights, weights)
-        self.assertEqual(test_gmm.n_components, len(gaussians))
-        self.assertEqual(test_gmm.dimension, gaussian_1.dimension)
+        self.assertEqual(test_gmm.n_mix, len(gaussians))
+        self.assertEqual(test_gmm.n_features, gaussian_1.n_features)
 
     def test_gmm_pdf(self):
-        """Tests if GaussianMixture.pdf correctly calculates the pdf values for input coordinates."""
+        """Tests if GaussianMixtureModel.pdf correctly calculates the pdf values for input coordinates."""
         x = [[1.1, 2.2], [-1, -0.5], [99, 100]]
         gaussian_1 = models.MultivariateGaussian(mean=[0, 0], cov=[[1, 0], [0, 1]])
         gaussian_2 = models.MultivariateGaussian(mean=[1, 1], cov=[[2, 0], [0, 2]])
@@ -48,7 +49,7 @@ class TestModels(unittest.TestCase):
         gaussians = [gaussian_1, gaussian_2, gaussian_3]
         weights = [0.7, 0.2, 0.1]
 
-        test_gmm = models.GaussianMixture(components=gaussians, weights=weights)
+        test_gmm = models.GaussianMixtureModel(gaussians, weights)
         test_pdf = test_gmm.pdf(x)
         scipy_pdf = [
             0.7 * gaussian_1.pdf(xi) + \
@@ -60,13 +61,13 @@ class TestModels(unittest.TestCase):
         assert np.allclose(test_pdf, scipy_pdf)
 
     def test_gmm_get_means(self):
-        """Tests if GaussianMixture.get_means() correctly returns the set of means in the right shape."""
+        """Tests if GaussianMixtureModel.get_means() correctly returns the set of means in the right shape."""
         gaussian_1 = models.MultivariateGaussian(mean=[0, 0], cov=[[1, 0],[ 0, 1]])
         gaussian_2 = models.MultivariateGaussian(mean=[1, 1], cov=[[2, 0], [0, 2]])
         gaussian_3 = models.MultivariateGaussian(mean=[99, 99], cov=[[2, 1], [1, 2]])
         gaussians = [gaussian_1, gaussian_2, gaussian_3]
         weights = [0.7, 0.2, 0.1]
-        test_gmm = models.GaussianMixture(components=gaussians, weights=weights)
+        test_gmm = models.GaussianMixtureModel(gaussians, weights)
 
         expected_means = np.array([[0, 0],
                                    [1, 1],
@@ -76,13 +77,13 @@ class TestModels(unittest.TestCase):
         assert np.allclose(test_gmm.get_means(), expected_means)
 
     def test_gmm_get_covs(self):
-        """Tests if GaussianMixture.get_covs() correctly returns the set of covariances in the right shape."""
+        """Tests if GaussianMixtureModel.get_covs() correctly returns the set of covariances in the right shape."""
         gaussian_1 = models.MultivariateGaussian(mean=[0,0,0], cov=[[1, 0, 0], [0, 1, 0], [0, 0, 1]])
         gaussian_2 = models.MultivariateGaussian(mean=[1,1,1], cov=[[2, 0, 0], [0, 2, 0], [0, 0, 2]])
         gaussian_3 = models.MultivariateGaussian(mean=[99,99,99], cov=[[2, 1, 1], [1, 2, 1], [1, 1, 2]])
         gaussians = [gaussian_1, gaussian_2, gaussian_3]
         weights = [0.7, 0.2, 0.1]
-        test_gmm = models.GaussianMixture(components=gaussians, weights=weights)
+        test_gmm = models.GaussianMixtureModel(gaussians, weights)
 
         expected_covs = np.array([[[1, 0, 0],
                                     [0, 1, 0],
@@ -96,6 +97,53 @@ class TestModels(unittest.TestCase):
         
         self.assertEqual(test_gmm.get_covs().shape, expected_covs.shape)
         assert np.allclose(test_gmm.get_covs(), expected_covs)
+
+    def test_init_hmm(self):
+        """Tests if the instance variables are correctly set for HiddenMarkovModel()."""
+        gaussian_1 = models.MultivariateGaussian(mean=[0, 0], cov=[[1, 0],[ 0, 1]])
+        gaussian_2 = models.MultivariateGaussian(mean=[1, 1], cov=[[2, 0], [0, 2]])
+        gaussian_3 = models.MultivariateGaussian(mean=[99, 99], cov=[[2, 1], [1, 2]])
+        gaussian_4 = models.MultivariateGaussian(mean=[101, 101], cov=[[1, 0], [0, 1]])
+
+        gmm_1 = models.GaussianMixtureModel([gaussian_1, gaussian_2], [0.5, 0.5])
+        gmm_2 = models.GaussianMixtureModel([gaussian_3, gaussian_4], [0.7, 0.3])
+        gmm_3 = models.GaussianMixtureModel([gaussian_1, gaussian_3], [0.9, 0.1])
+        gmm_4 = models.GaussianMixtureModel([gaussian_2, gaussian_4], [0.2, 0.8])
+        
+        transmat = [[0.7, 0.1, 0.2, 0.0],
+                    [0.1, 0.3, 0.4, 0.2],
+                    [0.2, 0.4, 0.1, 0.1],
+                    [0.0, 0.2, 0.1, 0.7]]
+        test_hmm = models.HiddenMarkovModel(
+            components=[gmm_1, gmm_2, gmm_3, gmm_4],
+            startprob=[0.25] * 4,
+            transmat=transmat
+        )
+
+        self.assertEqual(test_hmm.n_components, 4)
+        self.assertEqual(test_hmm.n_mix, gmm_1.n_mix)
+        self.assertEqual(test_hmm.covariance_type, "full")
+        self.assertListEqual(test_hmm.components, [gmm_1, gmm_2, gmm_3, gmm_4])
+        self.assertEqual(test_hmm.n_features, 2)
+        self.assertListEqual(test_hmm.startprob_, [0.25] * 4)
+        assert np.allclose(test_hmm.transmat_, transmat)
+        assert np.allclose(
+            test_hmm.weights_, 
+            [[0.5, 0.5],[0.7, 0.3],[0.9, 0.1],[0.2, 0.8]]
+        )
+        assert np.allclose(
+            test_hmm.means_,
+            [[[0, 0],[1, 1]], [[99, 99],[101, 101]],
+             [[0, 0], [99, 99]], [[1, 1], [101, 101]]]
+        )
+        assert np.allclose(
+            test_hmm.covars_,
+            [[[[1, 0], [0, 1]], [[2, 0], [0, 2]]],
+             [[[2, 1], [1, 2]], [[1, 0], [0, 1]]],
+             [[[1, 0], [0, 1]], [[2, 1], [1, 2]]],
+             [[[2, 0], [0, 2]], [[1, 0], [0, 1]]]]
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
